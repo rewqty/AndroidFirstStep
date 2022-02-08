a48d46039869f5e0431bc533661ddd52d12e84c2
justholli <zolincska00075@gmail.com>
2022-02-08 20:03:08 +0500

    done

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.lab1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = findViewById<TextView>(R.id.textView)
+        val name = findViewById<TextView>(R.id.fullName)
+        val student = Student("Ivan Ivanov", 3)
+        val anotherStudent = Student("Dmitriy", 4)
+        view.text = student.fullName
+        name.text = anotherStudent.toString()
+
+    }
+}
+
+data class Student(
+    val fullName: String,
+    val course: Int
+) {
+    override fun toString(): String {
+        return fullName
     }
 }
